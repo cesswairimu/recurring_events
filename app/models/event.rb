b92fbd3d6b4b823a2c8ffd93369aa6601bd7f4b5
Cess <cessmbuguar@gmail.com>
2018-08-10 12:20:56 +0300

    :confused:

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+  serialize :recurring, Hash
+
+  def recurring=(value)
+    if RecurringSelect.is_valid_rule?(value)
+      super(RecurringSelect.dirty_hash_to_rule(value).to_hash)
+    else
+      super(nil)
+    end
+  end
 end
